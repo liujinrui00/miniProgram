8cfd6e8d96ccb7baac94111f2c384cbdbe6769fe
liujinrui00 <1219791181@qq.com>
2020-10-27 09:24:12 +0800

    修改提交

---
+++ pages/user/user.js
@@ -0,0 +1,20 @@
+// pages/user/user.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userinfo:{},
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    const userinfo=wx.getStorageSync("userinfo");
+    this.setData({
+      userinfo,
+    })
+  },
+})
